9a5b76809bea900f06423d25156ea6445b16a4dc
nicolas.nunez <nicolas.nunez@eventbrite.com>
2022-03-11 15:05:08 -0300

    Add alternative code

--- app/scheduling/application/campaign_scheduler.py
+++ app/scheduling/application/campaign_scheduler.py
@@ -16,3 +16,24 @@ def schedule_campaign(campaign_repo: CampaignRepo, cmd: ScheduleCommand) -> None
         raise Exception("Campaign does not exist")
     campaign._schedule_datetime = cmd.schedule_datetime
     campaign_repo.update(campaign)
+
+
+# Alternative
+# This is an example alternative which is CRUD oriented
+# We don't choose this alternative because it's very generic and does not represent the
+# real intention of the use case, we only want to modify the schedule time explictly
+# in a particular use case
+
+@dataclass(frozen=True)
+class UpdateInfo:
+    id: CampaignId
+    schedule_datetime: datetime
+    name: str
+
+def update_campaign(campaign_repo: CampaignRepo, info: UpdateInfo) -> None:
+    campaign = campaign_repo.find(info.id)
+    if not campaign:
+        raise Exception("Campaign does not exist")
+    campaign._schedule_datetime = info.schedule_datetime or campaign._schedule_datetime
+    campaign._name = info.name or campaign._name
+    campaign_repo.update(campaign)
