06032c498231de14a7c5d2a25c84be02bb31d8c3
niconunez96 <nicolas110996@gmail.com>
2022-03-12 10:17:28 -0300

    Adjust flask configuration

--- server.py
+++ server.py
@@ -1,14 +1,17 @@
 from flask import Flask
+from dotenv import load_dotenv
 
 from app.controller import campaign_endpoint
 from app.shared.infra.db import init_db
 from settings import init_logging
 
+load_dotenv()
 app = Flask(__name__)
 app.register_blueprint(campaign_endpoint)
 
 
+init_db()
+init_logging()
+
 if __name__ == "__main__":
-    init_db()
-    init_logging()
     app.run(host="127.0.0.1", port=8080)
